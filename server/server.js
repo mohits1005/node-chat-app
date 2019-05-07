ebe199cd888a5add184272057d32f1e8219531e8
mohit sharma <mohit.shar1005@gmail.com>
2019-05-07 10:11:35 +0530

    Express render template

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const http = require('http');
 const express = require('express')
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    // res.send('Hello World');
+    res.render(index);
 });
 server.listen(port, () => {
     console.log(`Started on port ${port}`);
