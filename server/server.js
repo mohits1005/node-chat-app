d5fcc88c97a333670b735e261b6cc8c20086c754
mohit sharma <mohit.shar1005@gmail.com>
2019-05-08 15:41:07 +0530

    added login + signup + authentication api calls

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
+require('./config/config.js')
+
+const _ = require('lodash')
+const path = require('path');
 const http = require('http');
 const express = require('express')
-const path = require('path');
+const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 3000;
+var { mongoose } = require('./db/mongoose');
+var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
+const { ObjectID } = require('mongodb');
 var app = express();
-var server = http.createServer(app);
+const port = process.env.PORT;
+app.use(bodyParser.json());
 const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
@@ -12,6 +20,40 @@ app.get('/', (req, res) => {
     // res.send('Hello World');
     res.render(index);
 });
-server.listen(port, () => {
+
+//creating user
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password'])
+    var user = new User(body);
+    user.save().then((user) => {
+        return user.generateAuthToken();
+        // res.send(user);
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+//testing middleware token
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
+//logging user
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(body.email, body.password).then((user) => {
+        // res.send(user);
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send('Error');
+    });
+    // res.send(body);
+});
+
+app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
